04767b22f0120d5d0f9106ee113d5b38995f6bd5
JoaaoPaulo <jpcampioloalmeida@gmail.com>
2026-09-23 14:22:53 -0300

    Add Card, Rank and Suit model classes

---
+++ src/blackjack/model/Card.java
@@ -0,0 +1,19 @@
+package blackjack.model;
+
+public class Card {
+    private final Suit suit;
+    private final Rank rank;
+
+    public Card(Rank rank, Suit suit){
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit(){
+        return suit;
+    }
+}
---
+++ src/blackjack/model/Suit.java
@@ -0,0 +1,5 @@
+package blackjack.model;
+
+public enum Suit {
+    DIAMONDS, SPADES, HEARTS, CLUBS;
+}
